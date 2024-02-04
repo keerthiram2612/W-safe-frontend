8b577e9d0374cb7d14b21b899efd1818fe9244b2
Keerthana <keerthiram2612@gmail.com>
2024-02-04 18:36:57 +0530

    adding the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div>
+
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </nav>
+  )
+}
