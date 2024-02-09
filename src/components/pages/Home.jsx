88886ca6d28793272cd9d660d77b1aec6596cdbf
Keerthana <keerthiram2612@gmail.com>
2024-02-09 19:56:12 +0530

    adding the routers

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const gotoAbout=()=>{
+   navigate('/about');
+  }
+  const navigate = useNavigate();
   return (
     <div>
       <div className='image'>
        <img  className = "image1"src='https://zonesafe.com/wp-content/uploads/2022/09/Safety-Journey-Image-for-Website-Article.jpg'/>
       </div>
       <div className='home-content'>
-        <p></p>
+        <p>Way-safe is a navigation app that can help drivers & persons stay on track and
+          <br/> get alerts about safety hazards on the road. It can detect road 
+          <br/>hazards, traffic, road conditions, and other alerts.</p>
       </div>
-      <div className='btn-grad'>More about
-      </div>
+      <button onClick={gotoAbout} className='btn-grad'>More about
+      </button>
      
     </div>
   )
