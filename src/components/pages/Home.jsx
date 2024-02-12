41e3413a489ebaeb1777f577d583c9f9420047ed
Keerthana <keerthiram2612@gmail.com>
2024-02-12 20:21:49 +0530

    installing the fontawesome icons

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faInstagram} from  '@fortawesome/free-brands-svg-icons'
 
 export default function Home() {
   const gotoAbout=()=>{
@@ -11,6 +14,7 @@ export default function Home() {
       <div className='image'>
        <img  className = "image1"src='https://zonesafe.com/wp-content/uploads/2022/09/Safety-Journey-Image-for-Website-Article.jpg'/>
       </div>
+
       <div className='home-content'>
         <p>Way-safe is a navigation app that can help drivers & persons stay on track and
           <br/> get alerts about safety hazards on the road. It can detect road 
@@ -18,6 +22,9 @@ export default function Home() {
       </div>
       <button onClick={gotoAbout} className='btn-grad'>More about
       </button>
+      <div className='icons'>
+      <FontAwesomeIcon className="instagram"icon={faInstagram} />
+      </div>
      
     </div>
   )
